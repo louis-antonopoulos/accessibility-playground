2ab77b7c682e27fa8c59e5f81e666b08a93c2fb0
Louis Antonopoulos <louis@thoughtbot.com>
2021-07-21 18:03:37 -0400

    Change how constraints are added on DetailVC
    Use NSLayoutConstraint.activate

--- Accessibility Playground/View Controllers/DetailViewController.swift
+++ Accessibility Playground/View Controllers/DetailViewController.swift
@@ -36,9 +36,12 @@ class DetailViewController: UIViewController {
     view.addSubview(label1)
 
     label1.translatesAutoresizingMaskIntoConstraints = false
-    label1.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-    label1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-    label1.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+
+    NSLayoutConstraint.activate([
+      label1.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+      label1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+      label1.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)
+    ])
   }
 
   private func setupTableViews() {
@@ -49,16 +52,19 @@ class DetailViewController: UIViewController {
 
       view.addSubview(tableView)
       tableView.translatesAutoresizingMaskIntoConstraints = false
-      tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-      tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+      NSLayoutConstraint.activate([
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+      ])
     }
 
-    tableView1.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 20).isActive = true
-    tableView2.topAnchor.constraint(equalTo: tableView1.bottomAnchor, constant: 20).isActive = true
-
-    tableView2.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-
-    tableView1.heightAnchor.constraint(equalTo: tableView2.heightAnchor).isActive = true
+    NSLayoutConstraint.activate([
+      tableView1.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 20),
+      tableView2.topAnchor.constraint(equalTo: tableView1.bottomAnchor, constant: 20),
+      tableView2.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      tableView2.heightAnchor.constraint(equalTo: tableView1.heightAnchor)
+    ])
   }
 }
 
