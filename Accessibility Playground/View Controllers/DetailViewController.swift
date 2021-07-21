4456f94430603bca50bb7470eeca6866bc1d429a
Louis Antonopoulos <louis@thoughtbot.com>
2021-07-21 18:03:13 -0400

    Change text on DetailVC label

--- Accessibility Playground/View Controllers/DetailViewController.swift
+++ Accessibility Playground/View Controllers/DetailViewController.swift
@@ -29,7 +29,7 @@ class DetailViewController: UIViewController {
   }
 
   private func setupLabel() {
-    label1.text = "Some Label"
+    label1.text = "A Header Label"
 
     label1.textColor = .black
 
